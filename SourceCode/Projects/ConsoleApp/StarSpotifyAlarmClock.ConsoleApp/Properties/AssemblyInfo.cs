06c9497657896f8e99dba758f6abfb71e7c6939e
Stellan Lindell <stiltet@gmail.com>
2017-02-22 13:37:28 +0100

    Updated nuget packages. Added version number when running app. New release.

--- SourceCode/Projects/ConsoleApp/StarSpotifyAlarmClock.ConsoleApp/Properties/AssemblyInfo.cs
+++ SourceCode/Projects/ConsoleApp/StarSpotifyAlarmClock.ConsoleApp/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
